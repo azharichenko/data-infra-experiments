3e73d56969f2df1d213929f7bf446ebb7a294023
Alex Zharichenko <azharichenko@gmail.com>
2023-01-01 06:48:32 -0500

    Further consolidation of libraries

---
+++ common/secretsman.py
@@ -0,0 +1,25 @@
+"""Secrets management"""
+
+import os
+import sqlite3
+
+import click
+
+__all__ = ["get_secret"]
+
+
+def _create_secrets_store():
+    pass
+
+def get_secret():
+    pass
+
+def store_secret():
+    pass
+
+@click.command()
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
